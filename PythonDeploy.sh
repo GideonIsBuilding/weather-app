1609b26acf30f069c4fede7995dc5dff3ddcc529
GideonIsBuilding <galosikhena@gmail.com>
2024-01-01 10:57:58 +0100

    commented the read -p line as it is not advisable in a script that is connected to the provisioning of vagrant machines

--- PythonDeploy.sh
+++ PythonDeploy.sh
@@ -3,9 +3,9 @@
 source .env
 source get_ssh_vars.sh
 
-read -p "Enter the desired directory name for the app: " new_directory
+# read -p "Enter the desired directory name for the app: " new_directory
 
-# new_directory="Weather App"
+new_directory="Weather App"
 
 green_echo() {
     echo -e "\e[32m$1\e[0m"
